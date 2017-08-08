8227361aed37a8a8fe0e6921821bbc243e2b7edb
旭康史 <asahi@iMac.local>
2017-08-08 14:58:32 +0900

    全頁大まかに完成。細かい調整はまだ

--- js/master.js
+++ js/master.js
@@ -1,7 +1,16 @@
 
 $(function() {
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    dots: true,
+    items: 1,
+    autoplay: true,
+  });
+
   $(".toggle_button").click(function() {
     console.log("hit!")
     $(this).next().slideToggle();
+    return false;
   });
 });
